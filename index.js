ec1640f0beb27b0e0b2cb18a04cdd8b97e7cf09c
Prakhar Verma <prakharverma1022@gmail.com>
2023-06-21 15:48:41 +0530

    fix first click not working icon-hamburgur-menu

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Header
 function toggleHiddenHeaderTabsCtr() {
     const hiddenHeaderTabsCtr = document.querySelector(".hidden-header-tabs-ctr");
-    if (hiddenHeaderTabsCtr.style.display == "none") {
-        hiddenHeaderTabsCtr.style.display = "flex";
-    } else {
-        hiddenHeaderTabsCtr.style.display = "none";
-    }
+    hiddenHeaderTabsCtr.classList.toggle("hide");
 }
